9dd9e3377d72f6d18c5fa3e6e3ede6ec3581bbbb
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-01-18 19:55:30 +0000

    git rm --cached --ignore-unmatch application.yml

--- dependency_connection.py
+++
@@ -1,8 +0,0 @@
-
-
-command1 = '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"',
-command2 = 'brew doctor',
-command3 = 'brew install blueutil',
-command4 = 'brew install brightness'
-  
-   
