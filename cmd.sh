6ec630043b6fe60435c95a2b0b667b9d27b083f0
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-01-22 21:48:59 +0000

    Create cmd.sh

---
+++ cmd.sh
@@ -0,0 +1,5 @@
+library (notifies)
+
+terminal-notifier -message "Hello, this is my message" -title "Message Title"
+
+osascript -e 'display notification "Notification text" with title "Notification Title" subtitle "Notification sub-title" sound name "Pop"'
