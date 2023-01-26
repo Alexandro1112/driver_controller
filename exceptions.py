69e82fc641901330c02a0938687e7bf8125e5fc4
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-01-26 11:16:59 +0300

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,63 @@
+"""
+|---------------------|
+|Individual exceptions for methods|
+|---------------------|
+"""
+class ApplicationNameError(NameError):
+     """
+     App with pointed out name not exist.
+     """
+
+class UseLinuxTerminal(OSError):
+     """
+    Linux version already working.
+    """
+
+
+class ApplicationNotExist(SystemError):
+     """
+    Application, which you point outed is not exist
+    """
+
+
+class UnsupportedFormat(FileExistsError):
+     """
+    Method screenshot support only ['png', 'jpg', 'ico', 'gif', 'pict'] formats.
+    """
+
+
+class ConfirmationError(TypeError):
+     """
+    If confirmation is [False].
+    """
+
+
+class InvalidExtension(NameError):
+     """
+    No extension specified.
+    """
+
+
+class LinuxWhileNotSupport(NotImplementedError):
+     """
+    TODO:
+    make linux version library in a future.
+    """
+
+
+class WifiNameConnectError(NameError):
+     """
+    Password or SSID/Wi-fi name of Network is not right.
+    """
+
+
+class ValueBrightnessError(ValueError, TypeError):
+     """
+    Value is not type [int].
+    """
+
+
+class WifiValueError(ValueError, BaseException):
+    """
+    There is no wi-fi network in util "bunch keys".
+    """
