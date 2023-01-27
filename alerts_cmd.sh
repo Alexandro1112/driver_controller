9ce1a060ecfda7c93183e4fb3f767aafdee7cf84
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-01-27 16:25:33 +0300

    Update alerts_cmd.sh

--- alerts_cmd.sh
+++ alerts_cmd.sh
@@ -1,5 +1,6 @@
-library (notifies)
+#(notifies)
 
 terminal-notifier -message "Hello, this is my message" -title "Message Title"
 
 osascript -e 'display notification "Notification text" with title "Notification Title" subtitle "Notification sub-title" sound name "Pop"'
+echo Successful
