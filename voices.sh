937a94d9c195527a23ca240be9b3331d9e8809f0
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-01-28 20:44:26 +0300

    Create voices.sh

---
+++ voices.sh
@@ -0,0 +1,79 @@
+# Main voices
+say -v Alex -i Hello!
+say -v Alice -i Hello!
+say -v Alva -i Hello!
+say -v Amelie -i Hello!
+say -v Anna -i Hello!
+say -v Fiona -i Hello!
+say -v Fred -i Hello!
+say -v Ioana -i Hello!
+
+# Voices for python code.
+
+"""
+Agnes               en_US
+Albert              en_US
+Alex                en_US
+Alice               it_IT
+Alva                sv_SE
+Amelie              fr_CA
+Anna                de_DE
+Bad News            en_US
+Bahh                en_US
+Bells               en_US
+Boing               en_US
+Bruce               en_US
+Bubbles             en_US
+Carmit              he_IL
+Cellos              en_US
+Damayanti           id_ID
+Daniel              en_GB
+Deranged            en_US
+Diego               es_AR
+Ellen               nl_BE
+Fiona               en-scotland
+Fred                en_US
+Good News           en_US
+Hysterical          en_US
+Ioana               ro_RO
+Joana               pt_PT
+Junior              en_US
+Kanya               th_TH
+Karen               en_AU
+Kathy               en_US
+Kyoko               ja_JP
+Laura               sk_SK
+Lekha               hi_IN
+Luciana             pt_BR
+Mariska             hu_HU
+Mei-Jia             zh_TW
+Melina              el_GR
+Milena              ru_RU
+Moira               en_IE
+Monica              es_ES
+Nora                nb_NO
+Paulina             es_MX
+Pipe Organ          en_US
+Princess            en_US
+Ralph               en_US
+Samantha            en_US
+Sara                da_DK
+Satu                fi_FI
+Sin-ji              zh_HK
+Tarik               ar_SA
+Tessa               en_ZA
+Thomas              fr_FR
+Ting-Ting           zh_CN
+Trinoids            en_US
+Veena               en_IN
+Vicki               en_US
+Victoria            en_US
+Whisper             en_US
+Xander              nl_NL
+Yelda               tr_TR
+Yuna                ko_KR
+Zarvox              en_US
+Zosia               pl_PL
+Zuzana              cs_CZ
+"""
+
