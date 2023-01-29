69977099ac76044b3e0673cb410b24eaeab9c4f6
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-01-30 00:56:02 +0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,1 +1,8 @@
 
+# Complete this code until use library
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install ffmpeg
+brew install blueutil
+brew install brightness
+brew install terminal-notifier
