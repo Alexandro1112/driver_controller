db14266dff71ca54ae2f56e971700f98073e8229
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-02-14 10:15:42 +0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,8 @@
 
-# Complete this code until used library
+# Complete this code until used the library
 
-
+cd ~ 
+# ܿܿܿܿܿܿܿܿܿܿܿܿܿܿܿܿReplce ~ to working path to directory 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install ffmpeg
 brew install blueutil
