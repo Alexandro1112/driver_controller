5ba2ef39a3dd69d5c191038dae13bf6027f9e5a4
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-02-16 17:38:45 +0300

    Update info.py

--- info.py
+++ info.py
@@ -2,11 +2,13 @@
 # ----------------------- #
 __author__ = 'AlexAndro1112'
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
+
+__author_email__ = 'aleksandrbosov872@gmail.com'
+
 
 __all__ = [
-     '__version__', '__author__'
-]
+     '__version__', '__author__', '__author_email__']
 
 if __name__ == '__main__':
     pass
