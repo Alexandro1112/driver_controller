da520f6cce4305ea5bd65d9ff2b186de1fe31287
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-02-22 23:50:06 +0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,10 +1,30 @@
+/usr/bin/sh
+$ response=' '
 
-# Complete this code until used the library
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 
-cd ~ 
-# ܿܿܿܿܿܿܿܿܿܿܿܿܿܿܿܿReplce ~ to working path to directory 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install ffmpeg
-brew install blueutil
-brew install brightness
-brew install terminal-notifier
+plt=$(uname)
+if [[ "$plt" == 'Linux' ]]; then
+    $ response='Your Platform:Ubuntu(Linux): Wait installation..'
+    cd ~
+    # ܿܿܿܿܿܿܿܿܿܿܿܿܿܿܿܿReplace '~' to working path to directory
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install ffmpeg
+    brew install blueutil
+    brew install brightness
+    brew install terminal-notifier && echo -e "${GREEN} Successful!"
+elif [[ "$plt" == 'Darwin' ]]; then
+    $ responses= 'Your Platform:Mac-os: Wait installation..'
+    cd /Users/admin/PycharmProjects/~
+    # ܿܿܿܿܿܿܿܿܿܿܿܿܿܿܿܿReplace '~' to working path to directory
+    brew install ffmpeg
+    brew install blueutil
+    brew install brightness
+    brew install terminal-notifier && echo -e "${GREEN} Successful!"
+
+
+else
+    echo "${YELLOW} For windows no dependencies. Run Code."
+    exit 1;
+ fi
