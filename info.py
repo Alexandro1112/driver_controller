d7f76588afa63c2676a09ace05e2445d0f61c151
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-02-22 23:56:10 +0300

    Update info.py

--- info.py
+++ info.py
@@ -6,9 +6,9 @@ __version__ = '1.0.3'
 
 __author_email__ = 'aleksandrbosov872@gmail.com'
 
-
+__year_create__ = '2022'
 __all__ = [
-     '__version__', '__author__', '__author_email__']
+     '__version__', '__author__', '__year_create__', '__author_email__']
 
 if __name__ == '__main__':
     pass
