b37e676024ae470da10584fd80422269fd4f61c0
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-03-04 19:08:44 +0300

    Update _mac_engine.py

--- _mac_engine.py
+++ _mac_engine.py
@@ -1279,6 +1279,59 @@ if sys.platform == 'darwin' and int(platform.mac_ver()[0].split('.')[0]) > 8 and
                     newStrIng = AppKit.NSString.stringWithString_(text)
                     newData = newStrIng.nsstring().dataUsingEncoding_(AppKit.NSUTF8StringEncoding)
                     pb.setData_forType_(newData, init)
+                   
+          class Illumination:
+            """Change Illumination for keys"""
+               def increase_illumination(self):
+                    def doKey(down):
+
+                         NSSystemDefined = 14
+
+                         DECODE_TO_INT_KEY_F6 = 21
+
+
+                         ev = Quartz.NSEvent.otherEventWithType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2_(
+                              NSSystemDefined,  # type
+                              (0, 0),  # location
+                              0xa00 if down else 0xb00,  # flags
+                              0,  # timestamp
+                              0,  # window
+                              0,  # ctx
+                              8,  # subtype
+                              (DECODE_TO_INT_KEY_F6  << 16) | ((0xa if down else 0xb) << 8),  # data1
+                              -1  # data2
+                         )
+                         cev = ev.CGEvent()
+                         Quartz.CGEventPost(0, cev)
+
+                    doKey(True)
+                    doKey(False)
+
+               def decrease_illumination(self):
+                    def doKey(down):
+                         # NSEvent.h
+                         NSSystemDefined = 14
+
+                         DECODE_TO_INT_KEY_F5 = 22
+
+
+                         ev = Quartz.NSEvent.otherEventWithType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2_(
+                              NSSystemDefined,  # type
+                              (0, 0),  # location
+                              0xa00 if down else 0xb00,  # flags
+                              0,  # timestamp
+                              0,  # window
+                              0,  # ctx
+                              8,  # subtype
+                              (DECODE_TO_INT_KEY_F5 << 16) | ((0xa if down else 0xb) << 8),  # data1
+                              -1  # data2
+                         )
+                         cev = ev.CGEvent()
+                         Quartz.CGEventPost(0, cev)
+
+                    doKey(True)
+                    doKey(False)
+
 else:
      raise SystemError('Use python version more [3.7] and mac version [10.9] an more.')
 
