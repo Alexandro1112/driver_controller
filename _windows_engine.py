d5acf1866769ccaf1c738ce9d39d943eeeab5984
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-03-05 17:37:14 +0300

    Update _windows_engine.py

--- _windows_engine.py
+++ _windows_engine.py
@@ -9,9 +9,12 @@ if sys.platform.lower() == 'win32':
     class WindowsCmd(object):
         class Notifier(object):
             def __init__(self):
-                self.center_message ='msg * "%s"'
-            def send_text_message(self, text):
-                subprocess.getoutput(cmd=self.center_message % text)
+                STOP_ICON = 0x10
+                WARNING_ICON = 0x30
+                QUESTION_ICON 0x20
+                self.center_message = 'powershell (New-Object -ComObject Wscript.Shell).Popup("""%s""",0,"""%s""",%s)'
+            def send_text_message(self, text, label, icon):
+                subprocess.getoutput(cmd=self.center_message % (text, label, icon))
         class Password:
             def show_password_wifi(self, name_wifi_network):
                 if subprocess.getstatusoutput(cmd=f'netsh wlan show profile name={name_wifi_network} key=clear')[0] == 1:
