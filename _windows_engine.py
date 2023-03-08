1516caf5cee9e7423311342ac333d1c8e33bfa40
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-03-08 20:54:27 +0300

    Update _windows_engine.py

--- _windows_engine.py
+++ _windows_engine.py
@@ -11,23 +11,24 @@ if sys.platform.lower() == 'win32':
             def __init__(self):
                 STOP_ICON = 0x10
                 WARNING_ICON = 0x30
-                QUESTION_ICON 0x20
+                QUESTION_ICON  = 0x20
                 self.center_message = 'powershell (New-Object -ComObject Wscript.Shell).Popup("""%s""",0,"""%s""",%s)'
             def send_text_message(self, text, label, icon):
                 subprocess.getoutput(cmd=self.center_message % (text, label, icon))
         class Password:
             def show_password_wifi(self, name_wifi_network):
-                if subprocess.getstatusoutput(cmd=f'netsh wlan show profile name={name_wifi_network} key=clear')[0] == 1:
-                    raise WifiValueError(f'Can not find wifi-network {repr(name_wifi_network)}')
-                else:
-                    password = subprocess.getoutput(cmd=f'netsh wlan show profile name={name_wifi_network} key=clear')
-                    return password.strip()
+                 
+                 if subprocess.getstatusoutput(cmd=f'netsh wlan show profile name={name_wifi_network} key=clear')[0] == 1:
+                     raise WifiValueError(f'Can not find wifi-network {repr(name_wifi_network)}')
+                 else:
+                     password = subprocess.getoutput(cmd=f'netsh wlan show profile name={name_wifi_network} key=clear')
+                     return password.strip()
        class Brightness:
-           def set_brightness(self, brighrness):
+           def set_brightness(self, brightness):
                 if type(brightness) != int or type(brightness) != float:
                     raise ValueBirghtnessError('Brightness must be type:', int, float)
                 cmd = 'powershell (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,%s)'
-                subprocces.getoutput(cmd=cmd)
+                subprocess.getoutput(cmd=cmd)
 else:
     raise OSError( "" )
 
