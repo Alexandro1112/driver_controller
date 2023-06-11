7f94d4eea0ff963d1b7c940885f68fc9205e3d29
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-06-11 11:32:31 +0300

    Update illumination.py

--- macos/illumination.py
+++ macos/illumination.py
@@ -1,47 +1,24 @@
+import objc
+
+
+def load():
+    objc.loadBundle('CoreBrightness', bundle_path='/System/Library/PrivateFrameworks/CoreBrightness.framework',
+                    module_globals=globals())
+
+
 class Illumination:
-    def increase_illumination(self):
-        def doKey(down):
-            NSSystemDefined = 14
+    """Illumination object.
+    Connect keyboard and CoreBrightness API.
+    """
+    def setBrightness(self, level: float):
+        """set brightness on keyboard."""
+        load()
+        brightness = globals()['KeyboardBrightnessClient'].alloc().init().setBrightness_forKeyboard_(level, True)
+        if not bool(brightness):
+            raise ValueError('Can not set level of illumination with value equal {}'.format(level))
+        else:
+            return 0
 
-            DECODE_TO_INT_KEY_F6 = 21
-
-            ev = Quartz.NSEvent.otherEventWithType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2_(
-                NSSystemDefined,
-                (0, 0),
-                0xa00 if down else 0xb00,
-                0,
-                0,
-                0,
-                8,
-                (DECODE_TO_INT_KEY_F6 << 16) | ((0xa if down else 0xb) << 8),
-                -1
-            )
-            cev = ev.CGEvent()
-            Quartz.CGEventPost(0, cev)
-
-        doKey(True)
-        doKey(False)
-
-    def decrease_illumination(self):
-        def doKey(down):
-            # NSEvent.h
-            NSSystemDefined = 14
-
-            DECODE_TO_INT_KEY_F5 = 22
-
-            ev = Quartz.NSEvent.otherEventthType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2_(
-                NSSystemDefined,  # type
-                (0, 0),  # location
-                0xa00 if down else 0xb00,  # flags
-                0,  # timestamp
-                0,  # window
-                0,  # ctx
-                8,  # subtype
-                (DECODE_TO_INT_KEY_F5 << 16) | ((0xa if down else 0xb) << 8),  # data1
-                -1  # data2
-            )
-            cev = ev.CGEvent()
-            Quartz.CGEventPost(0, cev)
-
-        doKey(True)
-        doKey(False)
+    def getBrightness(self):
+        load()
+        return round(globals()['KeyboardBrightnessClient'].alloc().init().brightnessForKeyboard_(True))
