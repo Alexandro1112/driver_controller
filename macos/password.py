90e35d9cc5c715b4f6fdfd3fd36ca20802a016df
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2025-03-31 23:21:03 +0300

    Update password.py

--- macos/password.py
+++ macos/password.py
@@ -1,12 +1,21 @@
-import subprocess
 
-class PasswordManager(object):
-    def show_password_wifi(self, name_wifi_network):
-        password = subprocess.getoutput(cmd=f'security find-generic-password -wa {name_wifi_network}')
-        if name_wifi_network in subprocess.getoutput(cmd='/System/Library/PrivateFrameworks/Apple80211.'
-                                                         'framework/Versions/A/Resources/airport scan') or \
-                subprocess.getstatusoutput(cmd=f'security find-generic-password -wa {name_wifi_network}')[
-                    0] == 0:
-            return password.strip()
+from Security import (SecItemCopyMatching, kSecClass, kSecAttrAccount, kSecAttrService, kSecReturnData,
+                      kSecClassGenericPassword, kSecPropertyTypeSuccess)
+
+
+class PasswordManager:
+    def get_wifi_password(self, ssid):
+        # Define the query for the Keychain
+        query = {
+            kSecClass: kSecClassGenericPassword,
+            kSecReturnData: True,
+            kSecAttrAccount: ssid
+        }
+    
+        status, data = SecItemCopyMatching(query, None)
+        if status == 0:
+            psw = data.bytes().tobytes()
+            return psw.decode()
         else:
-            raise ValueError(f'Can not find wifi-network {repr(name_wifi_network)} in key chains.')
+            return None
+    
