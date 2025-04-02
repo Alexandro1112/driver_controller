3a3e7e2386ba07746f5ec0363842fd133921fee7
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2025-04-02 22:05:51 +0300

    Update theme.py

--- macos/theme.py
+++ macos/theme.py
@@ -1,17 +1,15 @@
 import subprocess
 from time import sleep
+from AppKit import NSUserDefaults
 
 class Theme:
     def __init__(self):
         self.cmd = 'osascript -e \'tell app "System Events" to tell appearance ' \
                    'preferences to set dark mode to not dark mode\''
 
-    def change_color_mode(self, pause: [int, float, complex]):
-        sleep(pause)
-        subprocess.getoutput(cmd=self.cmd)
+    def change_color_mode(self):
+        subprocess.getoutput(self.cmd)
 
     def current_theme(self):
         """Print current color mode on mac."""
-        if not 'Dark' in subprocess.getoutput('defaults find AppleInterfaceStyle'):
-            return "Light"
-        return "Dark"
+        return NSUserDefaults.standardUserDefaults().stringForKey_(u"AppleInterfaceStyle")
