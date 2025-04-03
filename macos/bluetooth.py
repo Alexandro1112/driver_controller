e41ce30d255da7488d15ac68aece692baf0156c7
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2025-04-03 10:29:01 +0300

    Update bluetooth.py

--- macos/bluetooth.py
+++ macos/bluetooth.py
@@ -6,18 +6,18 @@ class BlueTooth(object):
     def __init__(self):
         bundle_path = '/System/Library/Frameworks/IOBluetooth.framework'
 
-        dir(objc.loadBundle(objc.infoForFramework(bundle_path)[1], bundle_path=bundle_path,
-                            module_globals=globals()))
+        objc.loadBundle(objc.infoForFramework(bundle_path)[1], bundle_path=bundle_path,
+                            module_globals=globals())
 
         self.bluetooth = subprocess.getoutput(cmd='system_profiler SPBluetoothDataType')
 
         devices = globals()['IOBluetoothDevice']  # Ignore error.IOBluetoothDevice not objective-class,
         # and when I pack him in objc.loadBundle I
-        # got error, tied with subcridiable.
-        address = devices.recentDevices_(0)
+        # got error, based at subcridiable.
+        address = devices.recentDevices_(0) or devices.pairedDevices()
 
-        for Query_Devises in address:
-            self.device = Query_Devises
+        for query in address:
+            self.device = query 
 
     def get_paired_devices(self):
         return (self.device.getNameOrAddress())
@@ -33,16 +33,16 @@ class BlueTooth(object):
 
     def get_all_address(self):
         """[0] or [1]"""
-        ADDRESID = subprocess.getoutput(cmd='system_profiler SPBluetoothDataType | grep Address').split('Address')[1:]
-        ADDRESID = set(ADDRESID)
-        for ID in ADDRESID:
-            yield ID.rstrip().replace(': ', '').lstrip()
+        addr = subprocess.getoutput(cmd='system_profiler SPBluetoothDataType | grep Address').split('Address')[1:]
+        addr = set(addr)
+        for id in addr:
+            yield id.rstrip().replace(': ', '').lstrip()
 
     def pair_to_devise(self, address, duration_pair):
         try:
             device = globals()['IOBluetoothDevice'].withAddressString_(address)
             device.openConnection()
-            device.openConnection_withPageTimeout_authenticationRequired_(None, duration_pair, False)
+            device.openConnection_withPageTimeout_authenticationRequired_(None, duration_pair, True)
         except AttributeError:
             raise BluetoothConnectionError(f'Can not connect to devise, with address {repr(address)}')
 
